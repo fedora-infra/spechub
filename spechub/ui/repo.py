008c509faf7d7a17b6beebe3ada8152d6ce577ac
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-09-02 18:01:22 +0200

    Fix the repo controller to get the forks correctly

--- spechub/ui/repo.py
+++ spechub/ui/repo.py
@@ -109,7 +109,7 @@ def view_repo(repo, username=None):
         last_commits=last_commits,
         tree=tree,
         diff_commits=diff_commits,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
 
 
@@ -184,7 +184,7 @@ def view_repo_branch(repo, branchname, username=None):
         last_commits=last_commits,
         tree=sorted(last_commits[0].tree, key=lambda x: x.filemode),
         diff_commits=diff_commits,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
 
 
@@ -280,7 +280,7 @@ def view_log(repo, branchname='master', username=None):
         diff_commits=diff_commits,
         page=page,
         total_page=total_page,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
 
 
@@ -341,7 +341,7 @@ def view_file(repo, identifier, filename, username=None):
         filename=filename,
         content=content,
         output_type=output_type,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
 
 
@@ -391,7 +391,7 @@ def view_commit(repo, commitid, username=None):
         commit=commit,
         diff=diff,
         html_diff=html_diff,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
 
 
@@ -442,7 +442,7 @@ def view_tree(repo, identifier=None, username=None):
         filename='',
         content=content,
         output_type=output_type,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
 
 
@@ -464,5 +464,5 @@ def view_forks(repo, username=None):
         select='forks',
         username=username,
         repo=repo,
-        forks=spechub.lib.get_forks(SESSION, repo),
+        forks=spechub.lib.get_forks(SESSION, repo, user=username),
     )
